3586754bc6c3695472b7eb44ffb0a4271a7035d2
josue <joshrexy20@gmail.com>
2023-10-14 12:49:33 -0600

    Expression

--- expresiones.hs
+++ expresiones.hs
@@ -19,3 +19,10 @@ main = do
  print(evalua(Numero 101))
  print(5*2-4*3)
  print(evalua(Menos(Por(Numero 5)(Numero 2))((Por(Numero 4)(Numero 3)))))
+ --new expressions
+ print(4+6+1-4*7-2)
+ print(evalua(Menos(Menos(Mas(Mas(Numero 4)(Numero 6))(Numero 1))(Por(Numero 4)(Numero 7)))(Numero 2)))
+ print(10*5+10*2-3*2)
+ print(evalua(Menos(Mas(Por(Numero 10)(Numero 5))(Por(Numero 10)(Numero 2)))(Por(Numero 3)(Numero 2))))
+
+
